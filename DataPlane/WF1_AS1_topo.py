2216fec409da6715fd2d6afa7260384e08aa0c14
Ubuntu <amwangi254@VM-Mininet.jwdu0qsz2jyubhnbjdhzdivv0a.ax.internal.cloudapp.net>
2024-05-27 18:52:21 +0000

    updated topology

--- DataPlane/WF1_AS1_topo.py
+++ DataPlane/WF1_AS1_topo.py
@@ -1,31 +1,29 @@
+#!/usr/bin/env python3
 
 """
-Designing the offshore wind farm network topology using the spine-leaf network topology
-Author: Agrippina W. Mwangi
-Created on: April 2024
+    Designing the offshore wind farm network topology using the spine-leaf network topology
+    Author: Agrippina W. Mwangi
+    Created on: April 2024
 """
 
 from mininet.topo import Topo
-from mininet.node import OVSSwitch
-
+#from mininet.node import OVSSwitch
 
 class MyTopo(Topo):
-    
     # Create a constructor to launch STP that prevents network loops due to the spine-leaf mesh connections
-    def __init__(self):
-	Topo.__init__(self,switch=OVSSwitch, stp=True)
-	self.build()
-
+    #def __init__(self):
+       # Topo.__init__(self, switch=OVSSwitch, stp=True)
+        #self.build()
 
     # Creating the WF1_AS1 Network Topology
     def build(self):
-        '''
+        """
         Key:
-        1. LDAQ- Local Data acquisition, 
-        2. MU - Merging Unit, 
-        3. ECP - Edge Computing node, 
+        1. LDAQ- Local Data acquisition,
+        2. MU - Merging Unit,
+        3. ECP - Edge Computing node,
         4. vIED - virtual Intelligent Electronic Device
-        '''
+        """
 
         # Creating hosts
         ldaqwt1 = self.addHost('LDAQWT1', ip='192.168.1.10/24', defaultRoute='via 192.168.1.1')
@@ -52,10 +50,7 @@ class MyTopo(Topo):
         ecp4 = self.addHost('ECP4', ip='192.168.1.43/24', defaultRoute='via 192.168.1.1')
         ecp5 = self.addHost('ECP5', ip='192.168.1.44/24', defaultRoute='via 192.168.1.1')
 
-        '''
-        Creating the WT Nacelle and Tower switches, Spine and Leaf Switches
-        '''
-
+        # Creating the WT Nacelle and Tower switches, Spine and Leaf Switches
         wt1_sw1 = self.addSwitch('WT1_SW1')  # Nacelle Switch
         wt1_sw2 = self.addSwitch('WT1_SW2')  # Tower Switch
         wt2_sw1 = self.addSwitch('WT2_SW1')
@@ -66,55 +61,104 @@ class MyTopo(Topo):
         wt4_sw2 = self.addSwitch('WT4_SW2')
         wt5_sw1 = self.addSwitch('WT5_SW1')
         wt5_sw2 = self.addSwitch('WT5_SW2')
-        
-        #Connecting the local data acquisition nodes to the wind turbine nacelle access switches
-        self.addLink(ldaqwt1,wt1_sw1)
-        self.addLink(ldaqwt2,wt2_sw1)
-        self.addLink(ldaqwt3,wt3_sw1)
-        self.addLink(ldaqwt4,wt4_sw1)
-        self.addLink(ldaqwt5,wt5_sw1)
+
+        # Connecting the local data acquisition nodes to the wind turbine nacelle access switches
+        self.addLink(ldaqwt1, wt1_sw1)
+        self.addLink(ldaqwt2, wt2_sw1)
+        self.addLink(ldaqwt3, wt3_sw1)
+        self.addLink(ldaqwt4, wt4_sw1)
+        self.addLink(ldaqwt5, wt5_sw1)
 
         # Creating spine-leaf network topology
-        spine_switches = [self.addSwitch(f's{i}', cls=OVSSwitch, stp=True) for i in range(1, 5)]
-        leaf_switches = [self.addSwitch(f'l{i}', cls=OVSSwitch, stp=True) for i in range(1, 21)]
+        spine_switches = [self.addSwitch(f's{i}') for i in range(1, 6)]
+        leaf_switches = [self.addSwitch(f'l{i}') for i in range(1, 21)]
 
-        # Creating mesh connections between spine switches
-        for i in range(len(spine_switches)):
-            for j in range(i + 1, len(spine_switches)):
-                self.addLink(spine_switches[i], spine_switches[j])
+        #Connecting the wind turbine tower switch and spine switch
+        self.addLink(wt1_sw2,spine_switches[1])
+        self.addLink(wt2_sw2,spine_switches[2])
+        self.addLink(wt3_sw2,spine_switches[3])
+        self.addLink(wt4_sw2,spine_switches[4])
+        self.addLink(wt5_sw2,spine_switches[5])
+        
+        #Connecting all the spine switches to each other
+        self.addLink(spine_switches[1],spine_switches[2])
+        self.addLink(spine_switches[2],spine_switches[3])
+        self.addLink(spine_switches[3],spine_switches[4])
+        self.addLink(spine_switches[4],spine_switches[5])
+        
+        #Connecting each of the spine switches to all the distribution level leaf switches
+        self.addLink(spine_switches[1],leaf_switches[1])
+        self.addLink(spine_switches[1],leaf_switches[2])
+        self.addLink(spine_switches[1],leaf_switches[3])
+        self.addLink(spine_switches[1],leaf_switches[4])
+        self.addLink(spine_switches[1],leaf_switches[5])
 
-        # Creating mesh connections between leaf switches
-        for i in range(len(leaf_switches)):
-            for j in range(i + 1, len(leaf_switches)):
-                self.addLink(leaf_switches[i], leaf_switches[j])
+        self.addLink(spine_switches[2],leaf_switches[1])
+        self.addLink(spine_switches[2],leaf_switches[2])
+        self.addLink(spine_switches[2],leaf_switches[3])
+        self.addLink(spine_switches[2],leaf_switches[4])
+        self.addLink(spine_switches[2],leaf_switches[5])
 
-        # Connecting each spine switch to the first four leaf switches
-        for spine in spine_switches:
-            for leaf in leaf_switches[:4]:  # Only the first 4 leaf switches
-                self.addLink(spine, leaf)
+        self.addLink(spine_switches[3],leaf_switches[1])
+        self.addLink(spine_switches[3],leaf_switches[2])
+        self.addLink(spine_switches[3],leaf_switches[3])
+        self.addLink(spine_switches[3],leaf_switches[4])
+        self.addLink(spine_switches[3],leaf_switches[5])
 
-        # Linking all wt*_sw2 tower switches to each of the spine switches
-        tower_switches = [wt1_sw2, wt2_sw2, wt3_sw2, wt4_sw2, wt5_sw2]
-        for tower_switch in tower_switches:
-            for spine_switch in spine_switches:
-                self.addLink(tower_switch, spine_switch)
+        self.addLink(spine_switches[4],leaf_switches[1])
+        self.addLink(spine_switches[4],leaf_switches[2])
+        self.addLink(spine_switches[4],leaf_switches[3])
+        self.addLink(spine_switches[4],leaf_switches[4])
+        self.addLink(spine_switches[4],leaf_switches[5])
 
-        # Connect MU hosts to leaf switches l4 to l10
-        mu_hosts = [mu1, mu2, mu3, mu4, mu5]
-        for idx, mu_host in enumerate(mu_hosts):
-            leaf_index = 4 + idx  # Starting from leaf switch l4
-            self.addLink(mu_host, leaf_switches[leaf_index - 1])
+        self.addLink(spine_switches[5],leaf_switches[1])
+        self.addLink(spine_switches[5],leaf_switches[2])
+        self.addLink(spine_switches[5],leaf_switches[3])
+        self.addLink(spine_switches[5],leaf_switches[4])
+        self.addLink(spine_switches[5],leaf_switches[5])
 
-        # Connect ECP hosts to leaf switches l11 to l15
-        ecp_hosts = [ecp1, ecp2, ecp3, ecp4, ecp5]
-        for idx, ecp_host in enumerate(ecp_hosts):
-            leaf_index = 11 + idx  # Starting from leaf switch l11
-            self.addLink(ecp_host, leaf_switches[leaf_index - 1])
+        #connecting the distribution level leaf switches to the access level leaf switches
+        self.addLink(leaf_switches[1],leaf_switches[6])
+        self.addLink(leaf_switches[1],leaf_switches[7])
+        self.addLink(leaf_switches[1],leaf_switches[8])
 
-        # Connect vIED hosts to leaf switches l16 to l20
-        vied_hosts = [vied1, vied2, vied3, vied4, vied5]
-        for idx, vied_host in enumerate(vied_hosts):
-            leaf_index = 16 + idx  # Starting from leaf switch l16
-            self.addLink(vied_host, leaf_switches[leaf_index - 1])
+        self.addLink(leaf_switches[2],leaf_switches[9])
+        self.addLink(leaf_switches[2],leaf_switches[10])
+        self.addLink(leaf_switches[2],leaf_switches[11])
+
+        self.addLink(leaf_switches[3],leaf_switches[12])
+        self.addLink(leaf_switches[3],leaf_switches[13])
+        self.addLink(leaf_switches[3],leaf_switches[14])
+        
+        self.addLink(leaf_switches[4],leaf_switches[15])
+        self.addLink(leaf_switches[4],leaf_switches[16])
+        self.addLink(leaf_switches[4],leaf_switches[17])
+
+        self.addLink(leaf_switches[5],leaf_switches[18])
+        self.addLink(leaf_switches[5],leaf_switches[19])
+        self.addLink(leaf_switches[5],leaf_switches[20])
+        
+        #Connecting the Merging units in the OSS to the access level leaf switches
+        self.addLink(mu1,leaf_switches[6])
+        self.addLink(mu2,leaf_switches[7])
+        self.addLink(mu3,leaf_switches[8])
+        self.addLink(mu4,leaf_switches[9])
+        self.addLink(mu5,leaf_switches[10])
+
+        self.addLink(ecp1,leaf_switches[11])
+        self.addLink(ecp2,leaf_switches[12])
+        self.addLink(ecp3,leaf_switches[13])
+        self.addLink(ecp4,leaf_switches[14])
+        self.addLink(ecp5,leaf_switches[15])
+
+        self.addLink(vied1,leaf_switches[16])
+        self.addLink(vied2,leaf_switches[17])
+        self.addLink(vied3,leaf_switches[18])
+        self.addLink(vied4,leaf_switches[19])
+        self.addLink(vied5,leaf_switches[20])
+
+
 
 topos = {'mytopo': (lambda: MyTopo())}
+
+
