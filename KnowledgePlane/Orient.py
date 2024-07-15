64259e443a6b9c3e3985e9e237965db6443d6996
Agrippina Mwangi <97014828+PinaPhD@users.noreply.github.com>
2024-07-15 13:43:33 +0200

    Update Orient.py

--- KnowledgePlane/Orient.py
+++ KnowledgePlane/Orient.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 '''
-	Designing the offshore wind farm network topology using the spine-leaf network topology
+    Designing the offshore wind farm network topology using the spine-leaf network topology
     Author: Agrippina W. Mwangi
     Created on: July 2024
     ORIENT MODULE: Provides insights into the network traffic and triggers the DECIDE module
