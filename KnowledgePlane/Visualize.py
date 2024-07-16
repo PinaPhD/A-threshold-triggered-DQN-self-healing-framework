c01da1a3d4c3d3cb2f2363629cc0d48059939979
Agrippina Mwangi <97014828+PinaPhD@users.noreply.github.com>
2024-07-16 13:53:31 +0200

    Update and rename Orient.py to Visualize.py

--- KnowledgePlane/Visualize.py
+++ KnowledgePlane/Visualize.py
@@ -4,8 +4,7 @@
     Designing the offshore wind farm network topology using the spine-leaf network topology
     Author: Agrippina W. Mwangi
     Created on: July 2024
-    ORIENT MODULE: Provides insights into the network traffic and triggers the DECIDE module
-    when the QoS/SLA thresholds are exceeded.
+    ORIENT MODULE: Visualizes the network topology (Data Plane remotely controller --HBC-- by the ONOS SDN Controller)
 '''
 
 
