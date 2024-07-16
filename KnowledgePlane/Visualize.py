d2bfac7b5e854248499ced4a25e1a480e0f39eb1
Agrippina Mwangi <97014828+PinaPhD@users.noreply.github.com>
2024-07-16 14:49:59 +0200

    Delete KnowledgePlane/Visualize.py

--- KnowledgePlane/Visualize.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-'''
-    Designing the offshore wind farm network topology using the spine-leaf network topology
-    Author: Agrippina W. Mwangi
-    Created on: July 2024
-    ORIENT MODULE: Visualizes the network topology (Data Plane remotely controller --HBC-- by the ONOS SDN Controller)
-'''
-
